27a39cfb79044424c61b468c9e8907892ea5a37d
k024c1021-DAIWEI <sugedior19280303@gmail.com>
2024-11-26 05:20:24 +0000

    なおし

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def index():
     return """
         <html><body>
         <form action="/hello" method="GET">
-          名前: <input type="text" name="name">
+          名前: <input type="text" name="name"><br>
+          ひとこと： <input type="text" name="comment"><br>
           <input type="submit" value="送信">
         </form>
         </body></html>
@@ -19,13 +20,16 @@ def hello():
     name = request.args.get('name')
     if name is None: name = '名無し'
 
+    comment = request.args.get('comment', '') 
+
     cuisines = ['フランス料理', 'イタリア料理', '中華料理', '日本料理']
     recommended_cuisine = random.choice(cuisines)
 
     return """
     <h1>{0}さん、こんにちは！</h1>
-    <p>本日のおすすめランチは「{1}」です！</p>
-    """.format(name, recommended_cuisine)
+    <p>ひとこと: {1}</p>
+    <p>本日のおすすめランチは「{2}」です！</p>
+    """.format(name, comment, recommended_cuisine)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
